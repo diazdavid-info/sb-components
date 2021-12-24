6b6637eb20256efc0fa2711278a020496176ec4c
David Diaz <diazdavid.info@gmail.com>
2021-12-24 09:49:42 +0100

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,34 @@
+import './mylabel.css'
+
+export interface MyLabelProps {
+    /**
+     * Contenido del label
+     */
+    label: string
+    /**
+     * Como de largo quieres que sea el label?
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3'
+    /**
+     * Quieres que el texto sea en mayúsculas?
+     */
+    allCaps?: boolean
+    /**
+     * Que tipo de color quieres?
+     */
+    color?: 'primary' | 'secondary' | 'tertiary'
+    /**
+     * Color rgb de la fuente
+     */
+    fontColor?: string
+}
+
+export const MyLabel = ({
+    label = 'No Label', size = 'normal', allCaps = false, color = 'primary', fontColor
+}: MyLabelProps) => {
+    return (
+        <span className={ `label ${ size } text-${ color }` } style={ { color: fontColor } }>
+          { (allCaps) ? label.toUpperCase() : label }
+      </span>
+    )
+}
